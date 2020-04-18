84270a7f1eff852acdd4e713021beed9a321b69d
Mukkesh20 <mukkesh2025@gmail.com>
2020-04-18 08:53:14 +0530

    4 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+ FirstNumber:number  ;
+ SecondNumber:number;
+ result: number;
+
+constructor(){
+
+}
+
+  settFirstNumber(firstNumber: number ){
+   
+    this.FirstNumber=firstNumber;
+  
+  }
+
+  settSecondNumber(secondNumber:number){
+   
+    
+    this.SecondNumber=secondNumber;
+  }
+
+  onSub(){
+    this.result= this.FirstNumber - this.SecondNumber;
+  }
+
+  onDiv(){
+    this.result= this.FirstNumber / this.SecondNumber;
+  }
+
+  onAdd(){
+    
+    this.result= +this.FirstNumber + +this.SecondNumber;
+
+  }
+
+  onMulti(){
+    this.result= this.FirstNumber * this.SecondNumber;
+  }
+
+  pause(){
+
+  }
 }
